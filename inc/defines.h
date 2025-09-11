c284cbf01b2d81702021a569e8fb41bba39fc906
Janne Valkama <janne.e.valkama@gmail.com>
2025-09-11 12:19:26 +0300

    midday updates

--- inc/defines.h
+++ inc/defines.h
@@ -13,4 +13,26 @@
 #ifndef DEFINES_H
 # define DEFINES_H
 
+# include <stdbool.h>
+# include <stdint.h>
+# include <sys/types.h>
+# include <stddef.h>
+# include <limits.h>
+
+// 127 and 126 on bash error codeja, cmd not found, ja cannot execute
+// bashissa 126 cannot-execute triggeroityy yleensa jos bad permissions tai shebang puuttuu
+# define CMD_ERROR_NOTFOUND		127
+# define CMD_ERROR_CANNOTEX		126
+# define ERROR					-1
+# define SUCCESS				0
+
+typedef struct s_state			t_state;
+typedef struct s_cmd			t_cmd;
+
+typedef enum
+{
+	SIMPLE,
+	PIPELINE
+}	e_cmd_type;
+
 #endif
---
+++ inc/executor.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvalkama <jvalkama@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 10:33:34 by jvalkama          #+#    #+#             */
+/*   Updated: 2025/09/10 10:57:47 by jvalkama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTOR_H
+# define EXECUTOR_H
+
+# include "defines.h"
+# include <unistd.h>
+
+struct s_state
+{
+	int			pid_count;
+	pid_t		*pids;
+	int			exit_status; //edellisen exit status: tarvitaan $? komentoon
+	char		**env_var; //environment variableille
+	//whatever else is required to track globl status fo executions
+};
+
+struct s_cmd
+{
+	e_cmd_type		type;
+	char			**argv;
+	struct s_cmd	*left; //linked list kaytannossa.
+	struct s_cmd	*right;
+	int				exit_status;
+};
+
+#endif
